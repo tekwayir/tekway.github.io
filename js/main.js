cabb36cb77156d6dad613e45e8b85441943d58b5
alisouran <alirezasoran@gmai.com>
2019-07-12 20:12:34 +0430

    LAMP

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
 // smooth-scroll
-$.smoothScroll({
-    //滑动到的位置的偏移量
-    offset: 0,
-    //滑动的方向，可取 'top' 或 'left'
-    direction: 'top',
-    // 只有当你想重写默认行为的时候才会用到
-    scrollTarget: null,
-    // 滑动开始前的回调函数。`this` 代表正在被滚动的元素
-    beforeScroll: function () { },
-    //滑动完成后的回调函数。 `this` 代表触发滑动的元素
-    afterScroll: function () { },
-    //缓动效果
-    easing: 'swing',
-    //滑动的速度
-    speed: 700,
-    // "自动" 加速的系数
-    autoCoefficent: 2
-});
-
+// $.smoothScroll({
+//     //滑动到的位置的偏移量
+//     offset: 0,
+//     //滑动的方向，可取 'top' 或 'left'
+//     direction: 'top',
+//     // 只有当你想重写默认行为的时候才会用到
+//     scrollTarget: null,
+//     // 滑动开始前的回调函数。`this` 代表正在被滚动的元素
+//     beforeScroll: function () { },
+//     //滑动完成后的回调函数。 `this` 代表触发滑动的元素
+//     afterScroll: function () { },
+//     //缓动效果
+//     easing: 'swing',
+//     //滑动的速度
+//     speed: 700,
+//     // "自动" 加速的系数
+//     autoCoefficent: 2
+// });
 
 // Bind the hashchange event listener
 $(window).bind('hashchange', function (event) {
